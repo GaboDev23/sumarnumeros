ca16c7fb91e566b37efe2283ad628b5e5d5b76cc
gabog <gabo.gh12398@gmail.com>
2026-03-18 16:14:09 -0300

    Suma números con Pascal

---
+++ sumarnumeros.pas
@@ -0,0 +1,12 @@
+program sumarnumeros;
+var n1, n2, suma: Integer;
+begin
+ write('Ingresa un número: ');
+ readln(n1);
+ write('Ingresa otro número: ');
+ readln(n2);
+
+ suma := n1+n2;
+
+ writeln(n1, ' + ' ,n2, ' = ' ,suma)
+end.
